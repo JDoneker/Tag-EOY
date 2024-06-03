30f2e247064ef0390b3d0dbe26b158d0a2fd5f67
JDoneker <jtdoneker@gmail.com>
2024-06-03 05:10:15 -0500

    pushed to github
    pushed to github

--- src/Environment.java
+++ src/Environment.java
@@ -1,24 +1,45 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.util.Random;
+import java.util.Collections;
 
 public class Environment extends JFrame implements KeyListener, Runnable {
-	JLabel label;
+	JLabel label1,label2,label3,label4,label5;
 	ArrayList<PlayerFrame> PlayerFrames;
 	ArrayList<Integer> activeKeys;
+	private long startTime, curTime,timeLeft;
+	private boolean gameover;
+	private Sound sound;
 	public Environment() {
 		super("Environment");
 		this.setResizable(false);
+		this.setSize(40,100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sound = new Sound();
 		
 		PlayerFrames = new ArrayList<PlayerFrame>();
 		activeKeys = new ArrayList<Integer>();
+		startTime = System.currentTimeMillis();
+		gameover = false;
 		
-		label = new JLabel("hello");
-		this.add(label);
+		Container contentPane = this.getContentPane();
+		contentPane.setLayout(new GridLayout(0,1));
+		label1 = new JLabel("hello");
+		label2 = new JLabel("hello");
+		label3 = new JLabel("hello");
+		label4 = new JLabel("hello");
+		label5 = new JLabel("hello");
+		contentPane.add(label1);
+		contentPane.add(label2);
+		contentPane.add(label3);
+		contentPane.add(label4);
+		contentPane.add(label5);
 		
 		this.pack();
 		this.setLocationRelativeTo(null);
@@ -38,7 +59,39 @@ public class Environment extends JFrame implements KeyListener, Runnable {
 				for(int i = 0; i < activeKeys.size(); i++) {
 					checkKey(activeKeys.get(i));
 				}
-				label.setText(Long.toString(System.currentTimeMillis()));
+				
+				if(gameover){
+					ArrayList<Integer> scores = new ArrayList<>();
+					for(int i = 0; i<PlayerFrames.size(); i++) {
+						scores.add(PlayerFrames.get(i).getPanel().getScore());
+					}
+					int minValue = Collections.min(scores);
+					label1.setText("Player " + Integer.toString(scores.indexOf(minValue)+1) + " Wins");
+					
+					
+				}else{
+					curTime = System.currentTimeMillis()-startTime;
+					timeLeft = 30*1000-curTime;
+					if(timeLeft<0){
+						gameover = true;
+						sound.playmusic("smb_gameover.wav");
+					}
+					label1.setText("Time Left: "+Float.toString(timeLeft/1000));
+					if(PlayerFrames.size()>=1) {
+						label2.setText("Player 1 Score: " + Integer.toString(PlayerFrames.get(0).getPanel().getScore()));
+					}
+					if(PlayerFrames.size()>=2) {
+						label3.setText("Player 2 Score: " + Integer.toString(PlayerFrames.get(1).getPanel().getScore()));
+					}
+					if(PlayerFrames.size()>=3) {
+						label4.setText("Player 3 Score: " + Integer.toString(PlayerFrames.get(2).getPanel().getScore()));
+					}
+					if(PlayerFrames.size()>=4) {
+						label5.setText("Player 4 Score: " + Integer.toString(PlayerFrames.get(3).getPanel().getScore()));
+					}
+				}
+				
+				
 			}
 		}
 		catch(Exception e) {}
@@ -46,7 +99,12 @@ public class Environment extends JFrame implements KeyListener, Runnable {
 	public ArrayList<PlayerFrame> getPlayers(){
 		return PlayerFrames;
 	}
+public boolean getGameover(){
+	return gameover;
+}
 	public void start() {
+		Random random = new Random();
+		PlayerFrames.get(random.nextInt(PlayerFrames.size())).getPanel().setIt(true);
 		new Thread(this).start();
 		this.setFocusable(true);
 		this.requestFocus();
--- src/PlayerPanel.java
+++ src/PlayerPanel.java
@@ -10,30 +10,45 @@ import javax.swing.JPanel;
 public class PlayerPanel extends JPanel implements Runnable{
 	private BufferedImage back;
 	private ArrayList<Integer> KeyCodes;
-	private int x,y;
+	private float x,y;
+	private float vX, vY;
 	private Color PlayerColor;
 	private Environment E;
+	private boolean grounded;
+	private boolean it;
+	private int playerIndex;
+	private int score;
+	private int immunity;
+	private Sound sound;
 	
 	PlayerPanel(int i, ArrayList<Integer> keyCodes, Environment E1){
-		x = 0;
+		sound = new Sound();
+		playerIndex = i;
+		immunity = 0;
 		y = 0;
 		E = E1;
+		grounded = false;
+		it = false;
 		switch(i) {
 		case 0:
+			x = 0;
 			PlayerColor = Color.RED;
 			break;
 		case 1:
+			x = 500/4;
 			PlayerColor = Color.GREEN;
 			break;
 		case 2: 
+			x=500/2;
 			PlayerColor = Color.BLUE;
 			break;
 		case 3:
+			x = 1500/4;
 			PlayerColor = Color.ORANGE;
 			break;
 		}
 		KeyCodes = keyCodes;
-		this.setPreferredSize(new Dimension(500,320));
+		this.setPreferredSize(new Dimension(500,300));
 		this.setBackground(Color.GRAY);
 		back = null;
 
@@ -55,21 +70,59 @@ public class PlayerPanel extends JPanel implements Runnable{
 			back = (BufferedImage) (createImage(getWidth(),getHeight()));
 		}
 		Graphics g2d = back.createGraphics();
-		g2d.setColor(PlayerColor);
-		g2d.fillRect(0, 0, getSize().width, getSize().height);
-		g2d.clearRect(5,5,getSize().width-10,getSize().height-10);
+		g2d.clearRect(0,0,getSize().width,getSize().height);
+		if(E.getGameover()){
+			g2d.setColor(Color.BLACK);
+			g2d.drawString("Congrats",50,50);
+		}else{
+		
+		move();
+		
 		
 		ArrayList<PlayerFrame> playerFrames = E.getPlayers();
 		for(int i = 0; i<playerFrames.size(); i++) {
 			playerFrames.get(i).getPanel().drawPlayer(g2d);
 		}
-		
+		g2d.setColor(Color.BLACK);
+		if(it){
+			score++;
+			g2d.drawString("You are it", 50, 50);
+			for(int i = 0; i<playerFrames.size(); i++) {
+				if(collidesWith(playerFrames.get(i).getPanel())&& i != playerIndex&&!(playerFrames.get(i).getPanel().getImmunity()>0)){
+					immunity+=250;
+					it=false;
+					playerFrames.get(i).getPanel().setIt(true);
+					
+					
+				}
+			}
+		}
+		if(immunity>0){
+			immunity --;
+			g2d.drawString("You are immune", 50, 50);
+		}
+		}
 		twoDgraph.drawImage(back, 0, 0, null);
 		
 	}
+		public boolean collidesWith(PlayerPanel p) {
+            return this.x < p.getXpos() + 25 &&
+                   this.x + 25 > p.getXpos() &&
+                   this.y < p.getYpos() + 25 &&
+                   this.y + 25 > p.getYpos();
+    }
+	public void setIt(boolean b){
+		it = b;
+	}
+	public int getImmunity(){
+		return immunity;
+	}
+	public int getScore(){
+		return score;
+	}
 	public void drawPlayer(Graphics g2d) {
 		g2d.setColor(PlayerColor);
-		g2d.fillRect(x, y, 25, 25);
+		g2d.fillRect((int)x, (int)y, 25, 25);
 	}
 	public ArrayList<Integer> getKeyCodes(){
 		return KeyCodes;
@@ -81,9 +134,34 @@ public class PlayerPanel extends JPanel implements Runnable{
 		x--;
 	}
 	public void moveU() {
-		y--;
+		if(grounded){
+			sound.playmusic("break.wav");
+		vY=-5f;
+		grounded = false;
+		}
 	}
 	public void moveD() {
 		y++;
+	}
+	public void move(){
+		y+=vY;
+		x+=vX;
+		vY+=0.05;
+		if(y+25>getSize().height){
+			y=getSize().height-25;
+			grounded = true;
+		}
+		if(x<0){
+			x=0;
+		}
+		if(x+25>getSize().width){
+			x=getSize().width-25;
+		}
+	}
+	public float getXpos(){
+		return this.x;
+	}
+	public float getYpos(){
+		return this.y;
 	}
 }
